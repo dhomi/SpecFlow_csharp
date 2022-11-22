318970c3a24526a1ca404c3a65433512b49cb5f0
Beni DHomi <beni@qualityaccelerators.nl>
2022-11-22 11:46:33 +0100

    done

--- SpecFlow_01/StepDefinitions/Feature1StepDefinitions.cs
+++ SpecFlow_01/StepDefinitions/Feature1StepDefinitions.cs
@@ -1,5 +1,7 @@
 using RestSharp;
+using SpecFlow.Internal.Json;
 using System.Net;
+using System.Text.Json;
 using System.Threading;
 
 namespace SpecFlow_01.StepDefinitions
@@ -16,18 +18,32 @@ namespace SpecFlow_01.StepDefinitions
         [When(@"I request the get from api")]
         public async Task WhenIRequestTheGetFromApiAsync()
         {
-            string endpoint = "https://reqres.in/api";
-            var client = new RestClient(endpoint);
-            var request = new RestRequest("users/1", (Method)DataFormat.Json);
-            var response = await client.GetAsync(request);
-            Console.WriteLine("log.response" + response);
+            try
+            {
+                var endpoint = "https://reqres.in/api";
+                var client = new RestClient(endpoint);
+                var request = new RestRequest("users/1", Method.Get);
+                request.Timeout = 3000;
+                request.RequestFormat = DataFormat.Json;
+                request.OnBeforeDeserialization = resp => { resp.ContentType= "application/json"; };
+                var response = await client.ExecuteGetAsync(request);
+                Console.WriteLine("log.response => " + response.Content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exeption: " + e);
+            }
         }
 
         [Then(@"response status should be (.*)")]
         public void ThenResponseStatusShouldBe(int p0)
         {
             //throw new PendingStepException();
-            Console.WriteLine("given");
+            Console.WriteLine("\rgiven");
+        }
+
+        private interface IRestResponse
+        {
         }
     }
 }
